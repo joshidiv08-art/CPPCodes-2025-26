d1b923a09e97e3ce2939c1a7543ed17548719c98
joshidiv08-art <joshidiv08@gmail.com>
2026-05-04 01:10:04 +0530

    Create PrimeNo.cpp

---
+++ PrimeNo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout <<"Enter a number: ";
+    cin >>n;
+
+    if (n <= 1) 
+    {
+        cout << "Not a Prime number";
+        return 0;
+    }
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << "Not a Prime number";
+            return 0;
+        }
+    }
+
+    cout << "Prime number";
+    
+    return 0;
+}
